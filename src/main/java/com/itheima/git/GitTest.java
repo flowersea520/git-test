7c7e79efb577a62d3ea6000346324fd579bec5c7
mortal <2894266590@qq.com>
2023-12-12 23:08:05 +0800

    first commit

---
+++ src/main/java/com/itheima/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+/**
+ * @author mortal
+ * @date 2023/12/12 23:02
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
